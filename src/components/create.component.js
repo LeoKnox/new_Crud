ac74fba6949330135e01e76a63f7e1bb176bfa1c
LeoKnox <LeoKnox@users.noreply.github.com>
2019-04-19 23:18:08 -0700

    Add files via upload

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -3,8 +3,33 @@ import React, { Component } from  'react';
 export default class Create extends Component {
     render() {
         return (
-            <div>
-                <p>Welcome to Create Component!!</p>
+            <div style={{marginTop: 10}}>
+                <h3>Add New Word</h3>
+                <form>
+                    <div className="form-group">
+                        <label>Add Word: </label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Add Definition</label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Option1</label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Option2</label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Option3</label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Enter Words" className="btn btn-primary"/>
+                    </div>
+                </form>
             </div>
         )
     }
